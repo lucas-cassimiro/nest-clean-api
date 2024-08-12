9bfd9c76312a68ab2ef053f5b6864598600ef9e6
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2024-08-12 18:42:17 -0300

    feat: validation of environment variables using Zod and ConfigService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { PrismaService } from './prisma/prisma.service'
 import { SignUpController } from './controllers/signup.controller'
+import { envSchema } from './env'
 
 @Module({
+    imports: [
+        ConfigModule.forRoot({
+            validate: (env) => envSchema.parse(env),
+            isGlobal: true,
+        }),
+    ],
     controllers: [SignUpController],
     providers: [PrismaService],
 })
