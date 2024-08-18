c9a6f73e1f034763c35b013f7cd23f5b5c66e400
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2024-08-18 15:13:37 -0300

    feat: add AuthenticateController, CreateQuestionController, FetchRecentQuestionsController and AuthModule on imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,10 @@ import { ConfigModule } from '@nestjs/config'
 import { PrismaService } from './prisma/prisma.service'
 import { SignUpController } from './controllers/signup.controller'
 import { envSchema } from './env'
+import { AuthModule } from './auth/auth.module'
+import { AuthenticateController } from './controllers/authentication.controller'
+import { CreateQuestionController } from './controllers/create-question.controller'
+import { FetchRecentQuestionsController } from './controllers/fetch-recent-question.controller'
 
 @Module({
     imports: [
@@ -10,8 +14,14 @@ import { envSchema } from './env'
             validate: (env) => envSchema.parse(env),
             isGlobal: true,
         }),
+        AuthModule,
     ],
-    controllers: [SignUpController],
+    controllers: [
+        SignUpController,
+        AuthenticateController,
+        CreateQuestionController,
+        FetchRecentQuestionsController,
+    ],
     providers: [PrismaService],
 })
 export class AppModule {}
